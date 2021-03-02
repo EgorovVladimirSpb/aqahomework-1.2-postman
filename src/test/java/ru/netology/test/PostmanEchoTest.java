c6f1b7a823e8633a5491ea025affc72fc28fe36b
Egorov Vladimir <egorov.vladimir.spb@gmail.com>
2021-03-02 14:40:44 +0300

    Edited PostmanEchoTest, test failure

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -1,4 +1,28 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalToObject;
+
 public class PostmanEchoTest {
+
+    @Test
+    void shouldCheckPostInquiry() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalToObject("some data111"))
+
+        ;
+
+
+    }
 }
